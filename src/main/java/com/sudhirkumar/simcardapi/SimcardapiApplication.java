ad8ecf43fa5af8027d7ec249407d1cb00a4d9c35
suku0721 <Samiksha@0731>
2022-08-15 16:36:24 +0530

    Initial commit

---
+++ src/main/java/com/sudhirkumar/simcardapi/SimcardapiApplication.java
@@ -0,0 +1,13 @@
+package com.sudhirkumar.simcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimcardapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimcardapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudhirkumar/simcardapi/SimcardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhirkumar.simcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimcardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
